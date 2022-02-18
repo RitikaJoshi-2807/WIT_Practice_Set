c3e10d27d31cec59bb6f8f6d4b5a502838fd1e8b
Ritika Joshi <99448254+RitikaJoshi-2807@users.noreply.github.com>
2022-02-18 10:25:49 +0530

    Add files via upload

---
+++ basic_input_output.py
@@ -0,0 +1,12 @@
+# write a python program to find the average of two numbers
+x=int(input ("Enter the value of a :"))
+y=int(input ("Enter the value of b :"))
+
+avg= (x+y)/2
+print ("The average of x and y is :",avg)
+
+#write a program to find the square of a number entered by the user
+w=int(input ("Enter the value of a :"))
+sqr= w*w
+print ("The sqr of w is :",sqr)
+
