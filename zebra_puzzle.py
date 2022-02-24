15f3447359d3d3a3b29c0979e29fb69301af2aea
Ritika Joshi <99448254+RitikaJoshi-2807@users.noreply.github.com>
2022-02-24 16:32:58 +0530

    Create zebra_puzzle.py

---
+++ zebra_puzzle.py
@@ -0,0 +1,10 @@
+cigarette_brand= ["Kools", "Chesterfields", "Old Gold", "Lucky Strike", 
+                "Parliaments"]
+house_color= ["red","green","ivory","blue","yellow"]
+house_owner= ["Englishman", "Ukrainian", "Norwegian", "Spaniard", "Japanese"]
+beverages =["coffee","water","tea","milk","orange juice"]
+owns = ["dog", "snails", "fox", "horse", "zebra"]
+def owns_zebra():
+    return "Japanese"
+def drinks_water():
+    return "Norwegian"
