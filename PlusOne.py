b0249c1cf9cb48fba63763bb245d8681e350bc24
Ritika Joshi <99448254+RitikaJoshi-2807@users.noreply.github.com>
2022-02-24 23:38:52 +0530

    Update PlusOne.py

--- PlusOne.py
+++ PlusOne.py
@@ -1,7 +1,6 @@
 class Solution(object):
     def plusOne(self, digits):
         sum=0
-        lis=[]
         for i in range(len(digits)):
             sum+=digits[i]*pow(10, len(digits)-1-i)
         return [int(i) for i in str(sum+1)]
