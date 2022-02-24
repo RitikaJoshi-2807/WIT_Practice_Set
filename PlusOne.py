3a5a19f53954159a702e3a42f93316fe6d40cb21
Ritika Joshi <99448254+RitikaJoshi-2807@users.noreply.github.com>
2022-02-24 23:37:26 +0530

    Create PlusOne.py

---
+++ PlusOne.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def plusOne(self, digits):
+        sum=0
+        lis=[]
+        for i in range(len(digits)):
+            sum+=digits[i]*pow(10, len(digits)-1-i)
+        return [int(i) for i in str(sum+1)]
+        
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
