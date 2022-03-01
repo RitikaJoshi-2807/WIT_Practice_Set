3d1fdb254763be99abe07d2c1295f8b7363c7851
Ritika Joshi <99448254+RitikaJoshi-2807@users.noreply.github.com>
2022-03-01 16:52:51 +0530

    Create multiply_strings.py

---
+++ multiply_strings.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        num1=int(num1)
+        num2=int(num2)
+        res=num1*num2
+        return str(res)
+    
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        
