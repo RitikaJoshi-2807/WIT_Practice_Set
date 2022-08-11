4e7951a956b9e0760e21a2bbab58448d12838531
Ritika Joshi <99448254+RitikaJoshi-2807@users.noreply.github.com>
2022-08-11 20:37:37 +0530

    Create add_strings.py

---
+++ add_strings.py
@@ -0,0 +1,8 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        num1 = int(num1)
+        num2 = int (num2)
+        x = int(num1+num2)
+        x=str(x)
+        return x
+        
